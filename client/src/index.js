65633df7481c71fcd598189e260d409b3304ab50
Randie B <randie.bemis@gmail.com>
2019-06-04 17:38:02 -0700

    render a loading spinner while getting users from graphql endpoint

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { request } from 'graphql-request';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const graphqlEndpoint = 'https://random-users-app.randie.now.sh';
+
+const usersQuery = `
+{
+  users {
+   picture { thumbnail }
+   name { first last }
+   email
+   phone
+  }
+}
+`;
+
+function Loading() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress />
+    </div>
+  );
+}
+
+function App() {
+  const [users, setUsers] = React.useState(null);
+
+  React.useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await request(graphqlEndpoint, usersQuery);
+    setUsers(response);
+  };
+
+  return !users ? <Loading /> : <div>Users</div>;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
